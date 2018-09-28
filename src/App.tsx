cce15e09de85e8b2a5e73ef16ffb1708d867d7ee
Дмитрий Буковский <dbukovskyi@exadel.com>
2018-09-28 14:16:14 +0300

    add redux persist functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { createBrowserHistory } from 'history';
 import * as React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react'
 import './App.css';
 import { Header } from './Containers';
 import * as Page from './Pages';
@@ -10,23 +12,29 @@ import { configureStore } from './store';
 
 const history = createBrowserHistory();
 const store = configureStore(history);
+const persistor = persistStore(store);
 
 class App extends React.Component {
   public render() {
+
+    console.log(store.getState());
+
     return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <React.Fragment>
-            <Header/>
-            <Switch>
-              <Route exact={true} path="/" component={Page.Home}/>
-              <Route path="/signin" component={Page.SignIn}/>
-              <Route path="/signup" component={Page.SignUp}/>
-              <Route path="*" component={Page.NotFound}/>
-            </Switch>
-          </React.Fragment>
-        </ConnectedRouter>
-      </Provider>
+      <Provider store={store} >
+        <PersistGate loading={null} persistor={persistor} >
+          <ConnectedRouter history={history} >
+            <React.Fragment >
+              <Header />
+              <Switch >
+                <Route exact={true} path="/" component={Page.Home} />
+                <Route path="/signin" component={Page.SignIn} />
+                <Route path="/signup" component={Page.SignUp} />
+                <Route path="*" component={Page.NotFound} />
+              </Switch >
+            </React.Fragment >
+          </ConnectedRouter >
+        </PersistGate >
+      </Provider >
     );
   }
 }
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -3,16 +3,21 @@ import { History } from 'history';
 import { applyMiddleware, createStore, Store } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { authEpic } from './authServices';
 import { devicesEpic } from './devicesServices';
 import firebaseApi from './firebaseApi';
 import rootReducer from './reducer';
-// import { persistStore, persistReducer } from 'redux-persist'
-// import storage from 'redux-persist/lib/storage'
 
 export const configureStore = (history: History): Store => {
 
   firebaseApi.initialize();
+
+  const persistConfig = {
+    key: 'root',
+    storage,
+  };
 
   const rootEpic = combineEpics(
     authEpic,
@@ -27,13 +32,16 @@ export const configureStore = (history: History): Store => {
 
   const enhancer = applyMiddleware(routerMiddleware(history), epicMiddleware);
 
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+
   const store = createStore(
-    connectRouter(history)(rootReducer),
+    connectRouter(history)(persistedReducer),
     process.env.REACT_APP_DEV
       ? composeWithDevTools(enhancer)
       : enhancer,
   );
 
   epicMiddleware.run(rootEpic);
+
   return store;
 };
